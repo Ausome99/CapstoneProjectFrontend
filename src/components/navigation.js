a2a5255a12b250b552691fe798c2dcab9899eb7a
austinmcdaniel <ausome99@gmail.com>
2020-07-21 09:35:45 -0600

    added nav bar buttons

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,6 +3,9 @@ import React from 'react';
 export default function navigation(props) {
     return (
         <div className='navigation-wrapper'>
+            <button>Save</button>
+            <button>Load</button>
+            <button>Logout</button>
         </div>
     )
 }
